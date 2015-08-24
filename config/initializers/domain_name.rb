2c823d6b5cfb11bd0c740a2ecf8a890223dd8479
Ryan Smith <zeepickle@gmail.com>
2015-08-23 19:36:00 -0700

    for testing

--- config/initializers/domain_name.rb
+++ config/initializers/domain_name.rb
@@ -1,2 +1,2 @@
 # set the domain name
-FEED_FEEDER_DOMAIN_NAME = "https://6dacbc15.ngrok.com/" # ie. "http://lateral.io/feed-feeder/"
+FEED_FEEDER_DOMAIN_NAME = "https://58b59a40.ngrok.com/" # ie. "http://lateral.io/feed-feeder/"
